749826221554eac43895b0b8032df63f035e096c
Asderdd <aliihsan.hatun@turkdevs.com>
2017-12-30 08:21:11 +0000

    nx569j: Remove ConfigPanel

--- device.mk
+++ device.mk
@@ -189,10 +189,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
-# ConfigPanel
-PRODUCT_PACKAGES += \
-    ConfigPanel
-
 # fingerprint pay
 PRODUCT_BOOT_JARS += \
     ifaa_fingerprint
